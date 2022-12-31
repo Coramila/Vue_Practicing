0f73fddffc26ed64dc0f65aef33b7645abc8c233
Camila Carvalho <carvalho.camiilaa@gmail.com>
2022-12-31 18:51:11 -0300

    first commit

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'PimeiroComponente',
+
+        data(){
+            return {
+                nome: "Camila",
+                profissao: "Programadora"
+            }
+        }
+    }
+
+</script>
