afbbaf96f9e04bbcb41a2981807cf5ba57437961
Camila Carvalho <carvalho.camiilaa@gmail.com>
2023-01-01 09:09:07 -0300

    LifeCycle and components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
  <PrimeiroComponente />
+ <LifeCycle/>
+ <pessoa/>
+ <Form/>
 </template>
 
 <script>
   import PrimeiroComponente from './components/PrimeiroComponente.vue'
+  import LifeCycle from './components/LifeCycle.vue'
+  import pessoa from './components/pessoa.vue'
+  import Form from './components/Form.vue'
   
   export default {
     name: 'App',
     components: {
-      PrimeiroComponente
-    }
+      PrimeiroComponente,
+      LifeCycle,
+      pessoa,
+      Form
+    },
+
   }
 </script>
---
+++ src/components/Info.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>Estou trabalhando no momento.</p>
+        <p>Utilizo as seguintes tecnologias:</p>
+        <ul>
+            <li>JS</li>
+            <li>PHP</li>
+            <li>Phyton</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'Info',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
