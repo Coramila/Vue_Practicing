5e972dbfcdf73e848b35fc2cc6750d24a11b7972
Camila Carvalho <carvalho.camiilaa@gmail.com>
2023-01-01 09:47:56 -0300

    diretivas: v-if, v-show, v-bind

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,21 +1,33 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Estou em busca de novas oportunidades</p>
         <p>Utilizo as seguintes tecnologias:</p>
         <ul>
             <li>JS</li>
             <li>PHP</li>
             <li>Phyton</li>
         </ul>
+
+        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+        <p>Para acessar meu portfólio <a v-bind:href="meu_link" target="blank5">clique aqui.</a></p>
+        <Picture/>
     </div>
 </template>
 
 <script>
+import Picture from './Picture.vue';
 export default{
     name: 'Info',
+    components: {
+        Picture
+    },
     data(){
         return{
-
+            esta_trabalhando: false,
+            mostrar_email: false,
+            email: 'Camila@email.com',
+            meu_link: 'https://github.com/Coramila'
         }
     }
 }
